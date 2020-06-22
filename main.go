0e73ab6251dab07f467e9b5ab964ae2515f692bd
Simo Aleksandrov <simo3003@me.com>
2020-06-22 14:45:33 +0300

    capture screen

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"strconv"
+
+	"github.com/go-vgo/robotgo"
 )
 
 func main() {
-	coords := os.Args[1:]
-	fmt.Println(coords)
+	x, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	y, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	w, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		panic(err)
+	}
+	h, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		panic(err)
+	}
+	robotgo.CaptureScreen(x, y, w, h)
 }
