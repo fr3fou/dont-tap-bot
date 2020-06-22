52d864f340c5b3d3283ed3d47177b1a37d6d3492
Simo Aleksandrov <simo3003@me.com>
2020-06-22 14:36:02 +0300

    move mouse

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fr3fou/dont-go
 
 go 1.14
+
+require github.com/go-vgo/robotgo v0.90.2
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-vgo/robotgo"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("start")
+	robotgo.MouseClick("left", true)
+	robotgo.MoveMouseSmooth(100, 200, 1.0, 100.0)
+	fmt.Println("end")
 }
